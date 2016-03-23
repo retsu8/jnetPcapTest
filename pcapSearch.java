2c406492842f7d30697b92e48b59feadc438dc86
Retsu8 <teck.geek@hotmail.com>
2016-03-22 21:11:26 -0600

    syn and ack
    syn vs ack printing

--- pcapSearch.java
+++ pcapSearch.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class pcapSearch{
     public String[] pcapArray = s.split(',"');
     public String[] passwords = null;
-    public String[] IP = null;
+    public String[] IPsyn = null;
+    public String[] IPacksyn = null;
     public String IPADDRESS_PATTERN ="(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
     public Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
     public Matcher matcher = pattern.matcher(ipString);
@@ -12,14 +15,25 @@ public class pcapSearch{
 
     }
     public class void ipAddressSYN(String pcap){
+        Array[] bucket = null;
         for(String testStr : pcapArray){
-            if (matcher.find()) {
-                IP.add(matcher.group());
-                if (testStr.indexOf("ACK") != -1){
-                    ACK++
+            if (testStr.indexOf("SYN+ACK") != -1){
+               if (matcher.find()) {
+                   IPacksyn.add(matcher.group());
+                }
+            }else if (testStr.indexOf("SYN") != -1){
+                if (matcher.find()) {
+                    IPsyn.add(matcher.group());
                 }
             }
         }
+        for(String ip : ipsyn)
+            if(Arrays.asList(bucket).contains(ip)){
+                bucket[ip]++;
+            }
+            else
+                bucket.add(ip);
+                bucket[ip]++;
     }
     public class void passFinder(String pcap){
             for(String testStr : pcapArray){
